22c9fa77b4af0d4df69bb7f4bb828f5be7152199
Joel Anderson <joelanderson000@users.noreply.github.com>
2024-03-22 14:34:32 -0600

    add navbar scroll effect

---
+++ main.js
@@ -0,0 +1,29 @@
+//scroll nav style
+window.addEventListener('scroll', () => {
+    document.querySelector('.header__container').classList.toggle('window-scrolled', window.scrollY > 0);
+    document.querySelector('header').classList.toggle('hide-content', window.scrollY > 0)
+})
+
+  const navbar = document.querySelector('.nav__links');
+  const openNavBtn = document.querySelector('#nav__toggle-open');
+  const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+
+  openNavBtn.addEventListener('click', openNav);
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+  }
+
+  closeNavBtn.addEventListener('click', closeNav);
+
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  });

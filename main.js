a047a116207f772bd27d16767edd3193c4056849
Joel Anderson <joelanderson000@users.noreply.github.com>
2024-03-23 17:53:44 -0600

    add responsive elements for small screens

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.addEventListener('scroll', () => {
     document.querySelector('header').classList.toggle('hide-content', window.scrollY > 0)
 })
 
+
 //Modal Gallery previews
 
 var img = document.querySelectorAll(".image-gallery li img");
@@ -22,4 +23,35 @@ img.forEach((imgA)=>{
 
 modal.onclick = function() {
   modal.style.display = "none"
+}
+
+
+//Hamburger menu for small screens
+if (window.innerWidth < 1024){
+
+
+  const nav = document.querySelector('.nav__links');
+  const openNavBtn = document.querySelector('#nav__toggle-open');
+  const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+
+  openNavBtn.addEventListener('click', openNav);
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+  }
+
+  closeNavBtn.addEventListener('click', closeNav);
+
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  });
+
 }

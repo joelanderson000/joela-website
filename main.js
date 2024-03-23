e0640b10096e31a0b6af47141f82784cf8ffeecf
Joel Anderson <joelanderson000@users.noreply.github.com>
2024-03-23 16:31:15 -0600

    add gallery section and modal for gallery images

--- main.js
+++ main.js
@@ -4,26 +4,24 @@ window.addEventListener('scroll', () => {
     document.querySelector('header').classList.toggle('hide-content', window.scrollY > 0)
 })
 
-  const navbar = document.querySelector('.nav__links');
-  const openNavBtn = document.querySelector('#nav__toggle-open');
-  const closeNavBtn = document.querySelector('#nav__toggle-close');
+//Modal Gallery previews
 
-  const openNav = () => {
-    nav.style.display = 'flex';
-    openNavBtn.style.display = 'none';
-    closeNavBtn.style.display = 'inline-block';
+var img = document.querySelectorAll(".image-gallery li img");
+var modal = document.getElementById("gallery-modal");
+var modalImg = document.getElementById("gallery-modal-img");
+var captionText = document.getElementById("gallery-modal-caption");
+
+img.forEach((imgA)=>{
+  imgA.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
   }
+});
 
-  openNavBtn.addEventListener('click', openNav);
 
-  const closeNav = () => {
-    nav.style.display = 'none';
-    openNavBtn.style.display = 'inline-block';
-    closeNavBtn.style.display = 'none';
-  }
-
-  closeNavBtn.addEventListener('click', closeNav);
-
-  nav.querySelectorAll('li a').forEach(navLink => {
-    navLink.addEventListener('click', closeNav);
-  });
+var span = document.getElementsByClassName("close-gallery-modal")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
